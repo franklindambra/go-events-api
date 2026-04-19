97a65bd69734fcdf2019e91f0956316e6196c92c
Franklin Dambra <franklindambra@gmail.com>
2026-04-18 20:52:35 -0500

    fetch single event

--- main.go
+++ main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	"strconv"
+	"time"
+
+	"example.com/rest-api/db"
 	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 	server.GET("/events", getEvents)
-	server.POST("/events")
+	server.GET("/events/:id", getEvent)
+	server.POST("/events", createEvent)
 	server.Run(":8000")
 }
 
 
 	func getEvents(ctx *gin.Context){
-		events := models.GetAllEvents()
+		events, err := models.GetAllEvents()
+		if err != nil {
+			ctx.JSON(500, gin.H{"message": "Could not getch events"})
+		}
+
 		ctx.JSON(200, events)
+	}
+
+
+	func getEvent(ctx *gin.Context){
+		eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(500, gin.H{"message": "Could not parse event ID"})
+		}
+		event, err := models.GetEventByID(eventId)
+		if err != nil {
+			ctx.JSON(500, gin.H{"message": "Could not find event"})
+		}
+		ctx.JSON(200, event)
 	}
 
 
@@ -23,13 +46,19 @@ func main() {
 		var event models.Event //var of type event
 		err :=ctx.ShouldBindJSON(&event) //bind req payload to type, can accept partial
 
-		if err!= nil {
+		if err != nil {
 			ctx.JSON(400, gin.H{"message": "could not parse request data"})
 			return
 		}
+		event.DateTime = time.Now()
 
-		event.ID  = 1
-		event.UserID = 1
+		err = event.Save()
+
+		if err != nil {
+			ctx.JSON(500, gin.H{"message": "Could not save event"})
+		}
+
+
 		ctx.JSON(200, gin.H{"message": "Event created", "event": event})
 	}
 
--- models/event.go
+++ models/event.go
@@ -1,25 +1,82 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"example.com/rest-api/db"
+)
 
 //binding struct tag works with the ShouldBindJSON method in main, err genreated if data missing required field
 type Event struct {
-	ID int
+	ID int64
 	Name string `binding:"required"`
-	Descrtiption string `binding:"required"`
+	Description string `binding:"required"`
 	Location string `binding:"required"`
-	DateTime time.Time `binding:"required"`
+	DateTime time.Time
 	UserID  int
 }
 
-var events []Event = []Event{}
 
 
-func (event Event)Save(){
-		events = append(events, event)
+func (event Event)Save()error{
+
+	query := `
+	INSERT INTO event(name, description, location, dateTime, user_id)
+	VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(event.Name, event.Description, event.Location, event.DateTime, event.UserID)
+	if err != nil{
+		return err
+	}
+	id, err := result.LastInsertId()
+	event.ID = id
+	return err
+
 }
 
 
-func GetAllEvents()[]Event{
-		return events
+func GetAllEvents()([]Event, error){
+		query := "SELECT * FROM event"
+		rows, err := db.DB.Query(query)
+		if err != nil {
+			return nil, err
+		}
+		defer rows.Close()
+		var events []Event
+		//.Next returns true as long as rows are left and then false thereafter
+		for rows.Next() {
+			var event Event
+			err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID )
+			if err != nil {
+				return nil, err
+			}
+			events = append(events, event )
+		}
+
+		return events, nil
+
+}
+
+
+func GetEventByID(eventId int64)(*Event, error){
+
+	query := `SELECT * FROM event WHERE id = ?`
+
+	row := db.DB.QueryRow(query, eventId)
+
+	var event Event
+
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID )
+
+	if err != nil {
+	 return nil, err
+	}
+
+
+	return &event, nil
+
 }
